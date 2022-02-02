3b9379651ff3ad7f855034102a6561f0c8946273
Victoria Lund <victoriavl@uia.no>
2022-02-02 13:00:54 +0100

    Added needed libraries

--- Puzzle_box-Bach/src/main.cpp
+++ Puzzle_box-Bach/src/main.cpp
@@ -1,4 +1,14 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+
+#include <DFRobotDFPlayerMini.h>
+#include <Adafruit_LEDBackpack.h>
+#include <DFRobot_I2CMultiplexer.h>
+#include <Adafruit_Keypad.h>
+#include <Adafruit_NeoPixel.h>
+#include <Adafruit_seesaw.h>
+#include <LiquidCrystal_I2C.h>
 
 void setup() {
   printf("Hello, World!");

8f9bd22108b8ae2af5973c3c4da7c0717bc050cb
roberh18 <roberh18@uia.no>
2022-02-08 14:27:51 +0100

    Roberh

---
+++ Puzzle_box-Bach/src/FtS.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+
+#include <DFRobotDFPlayerMini.h>
+#include <Adafruit_LEDBackpack.h>
+//#include <DFRobot_I2CMultiplexer.h>
+#include <Adafruit_Keypad.h>
+#include <Adafruit_NeoPixel.h>
+#include <Adafruit_seesaw.h>
+#include <LiquidCrystal_I2C.h>
+
+void setup() {
+  printf("Hello, World!");
+  //Hello again Roberh!
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}

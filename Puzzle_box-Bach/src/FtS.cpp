413f3e04b84c017acc88e4ea50d3b4212ce6e9af
roberh18 <roberh18@uia.no>
2022-02-10 16:54:33 +0100

    Roberh

--- Puzzle_box-Bach/src/FtS.cpp
+++ Puzzle_box-Bach/src/FtS.cpp
@@ -10,11 +10,28 @@
 #include <Adafruit_seesaw.h>
 #include <LiquidCrystal_I2C.h>
 
+const int buttonPin = 2;     // the number of the pushbutton pin
+//const int ledPin =  13;      // the number of the LED pin
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
 void setup() {
-  printf("Hello, World!");
-  //Hello again Roberh!
+ 
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // Print state:
+    Serial.print("HIGH");
+  } else {
+    // Print state:
+    Serial.print("LOW");
+  }
 }

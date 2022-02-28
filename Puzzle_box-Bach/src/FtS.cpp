8f71902b0d7738806f9b93e2d6ece0ad79bc9fd8
roberh18 <roberh18@uia.no>
2022-02-28 08:26:40 +0100

    roberh

--- Puzzle_box-Bach/src/FtS.cpp
+++ Puzzle_box-Bach/src/FtS.cpp
@@ -8,6 +8,11 @@
 #include <Adafruit_NeoPixel.h>
 #include <Adafruit_seesaw.h>
 #include <LiquidCrystal_I2C.h>
+#include <Adafruit_GFX.h>
+// #include <Adafruit_BusIO_Register.h>
+// #include <Adafruit_I2CRegister.h>
+// #include <Adafruit_SPIDevice.h>
+// #include <Adafruit_I2CDevice.h>
 
 const int BlueButtonPin = 0;    // Blue ToggleSwitch pin
 const int RedButtonPin = 1;     // Red ToggleSwitch pin
@@ -16,6 +21,12 @@ const int OrangeButtonPin = 3;  // Orange ToggleSwitch pin
 const int WhiteButtonPin = 4;   // White ToggleSwitch pin
 
 const int ButtonPins[5] = {0, 1, 2, 3, 4};
+int MyRegister[5] = {0};        // Saves the states of toggle switches in an array
+
+int Allow_Start = 1;            // Determines if the game can be started (start button cant be used as a reset after the game has started)
+int Fail = 0;                   // If fail != 0, it means the switch was toggled at the wrong moment (wrong number on timer)
+int Success = 0;                // Counts successful toggles 
+int Count = 0;                  // Counts the number of switches toggled / flipped
 
 // variables will change:
 int BlueButtonState = LOW;         // variable for reading the toggle switch status
@@ -33,6 +44,10 @@ int WhitePreviousState = LOW;   //Saves the previous state of the toggle switch
 
 unsigned long DebounceDelay = 50;
 unsigned long DebounceTimer = 0;
+unsigned int CountdownTimer;  //how much time the player has to solve the puzzle
+const unsigned long CountdownSpeed1 = 1000; // ticks down at 1000ms rate (normal speed)
+const unsigned long CountdownSpeed2 = 10;   // Timer ticks down at a faster rate after failure (wrong input)
+unsigned int TimerCountdownSpeed = CountdownSpeed1; //How mu
 
 //Switchen er NO, så, når den fysisk er switched ON så kobles pinnen til jord, og vi får ingen lys i LED. Derfor er det OFF state (siden LED da lyser) som skal registreres i koden, og brukes til å registrere at den er "PÅ", mens signalet egentlig er lavt
 
@@ -52,10 +67,14 @@ void DebounceBlue()
         {
           Serial.println("Blue switch OFF");
         }
-      else {Serial.println("Blue switch ON");}
+      if (BlueButtonState == HIGH)
+        {
+          Serial.println("Blue switch ON");
+        }
     }
   }
   BluePreviousState = BlueStateNow; 
+  MyRegister[0] = BlueStateNow; 
 }
 
 void DebounceRed()
@@ -74,10 +93,14 @@ void DebounceRed()
         {
           Serial.println("Red switch OFF");
         }
-      else {Serial.println("Red switch ON");}
+      if (RedButtonState == HIGH) 
+        {
+          Serial.println("Red switch ON");
+        }
     }
   }
-  RedPreviousState = RedStateNow; 
+  RedPreviousState = RedStateNow;
+  MyRegister[1] = RedStateNow; 
 }
 
 void DebounceGreen()
@@ -96,10 +119,14 @@ void DebounceGreen()
         {
           Serial.println("Green switch OFF");
         }
-      else {Serial.println("Green switch ON");}
+      if (GreenButtonState == HIGH) 
+        {
+          Serial.println("Green switch ON");
+        }
     }
   }
-  GreenPreviousState = GreenStateNow; 
+  GreenPreviousState = GreenStateNow;
+  MyRegister[2] = GreenStateNow; 
 }
 
 void DebounceOrange()
@@ -118,10 +145,14 @@ void DebounceOrange()
         {
           Serial.println("Orange switch OFF");
         }
-      else {Serial.println("Orange switch ON");}
+      if (OrangeButtonState == HIGH) 
+        {
+          Serial.println("Orange switch ON");
+        }
     }
   }
   OrangePreviousState = OrangeStateNow; 
+  MyRegister[3] = OrangeStateNow;
 }
 
 void DebounceWhite()
@@ -140,10 +171,14 @@ void DebounceWhite()
         {
           Serial.println("White switch OFF");   //Switchen er NO, og når den er switchen ON så kobles den til jord, så vi får ingen lys i LED. Derfor er det HIGH state (siden LED da lyser) som skal registreres i koden, og brukes til å registrere at den er "PÅ", mens signalet egentlig er lavt
         }
-      else {Serial.println("White switch ON");}
+      if (WhiteButtonState == HIGH) 
+        {
+          Serial.println("White switch ON");
+        }
     }
   }
-  WhitePreviousState = WhiteStateNow; 
+  WhitePreviousState = WhiteStateNow;
+  MyRegister[4] = WhiteStateNow; 
 }
 
 
@@ -165,6 +200,9 @@ DebounceOrange();
 DebounceWhite();
 
 }
+
+
+
 
 
 

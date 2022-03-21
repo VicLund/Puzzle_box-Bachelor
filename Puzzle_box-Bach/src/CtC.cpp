b25e306a1352fba002a1c927248bc74ee6b3827e
roberh18 <roberh18@uia.no>
2022-03-21 17:24:29 +0100

    roberh

--- Puzzle_box-Bach/src/CtC.cpp
+++ Puzzle_box-Bach/src/CtC.cpp
@@ -35,53 +35,96 @@ int                 MyRotaryRegister[3]     = {0, 0, 0};                        
 int                 RotaryPrevious[3]       = {0, 0, 0};                                         //
 int                 Rotary[3]               = {0, 1, 2};                                         // 
 int                 Action[3]               = {0, 0, 0};                                         //
-const unsigned int  Password1[4]            = {3, 7, 2, 5};                                //
-const unsigned int  Password2[4]            = {7, 0, 3, 4};                                //
+const unsigned int  Password1[4]            = {3, 7, 2, 5};                                      //
+const unsigned int  Password2[4]            = {7, 0, 3, 4};                                      //
 unsigned int        Digit[4]                = {0, 0, 0, 0};                                      //
 unsigned long       DebounceDelay           = 20;                                                // Debounce delay for toggle switches
 unsigned long       DebounceTimer           = 0;                                                 // Stores the current value of millis()   
-uint8_t             data[4]                 = {0xff, 0xff, 0xff, 0xff };                         //
-TM1637Display       display(CLK, DIO);                                                           //
-const uint8_t       SEG_DONE[]              = { 
-                                                SEG_B | SEG_C | SEG_D | SEG_E | SEG_G,           // d
-                                                SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,   // O
-                                                SEG_C | SEG_E | SEG_G,                           // n
-                                                SEG_A | SEG_D | SEG_E | SEG_F | SEG_G            // E
-                                              };
+uint8_t             data[4]                 = {0xff, 0xff, 0xff, 0xff };                         //                                                           //
+const uint8_t       SEG_SWIRL1[]            = {SEG_A, SEG_A, 0, 0};
+const uint8_t       SEG_SWIRL2[]            = {0, 0, SEG_A, SEG_A};
+const uint8_t       SEG_SWIRL3[]            = {0 ,0 ,0 ,SEG_B | SEG_C};
+const uint8_t       SEG_SWIRL4[]            = {0, 0, SEG_D, SEG_D};
+const uint8_t       SEG_SWIRL5[]            = {SEG_D, SEG_D, 0, 0};
+const uint8_t       SEG_SWIRL6[]            = {SEG_E | SEG_F, 0, 0, 0};
+const uint8_t       SEG_DONE[]              = {SEG_B | SEG_C | SEG_D | SEG_E | SEG_G, SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F, SEG_C | SEG_E | SEG_G, SEG_A | SEG_D | SEG_E | SEG_F | SEG_G};
+const uint8_t       SEG_PASS[]              = {SEG_A | SEG_B | SEG_E | SEG_F | SEG_G , SEG_A | SEG_B | SEG_C | SEG_E | SEG_F | SEG_G , SEG_A | SEG_C | SEG_D | SEG_G | SEG_F , SEG_A | SEG_C | SEG_D | SEG_G | SEG_F};
+const uint8_t       SEG_FAIL[]              = {SEG_A | SEG_F | SEG_E | SEG_G , SEG_A | SEG_B | SEG_C | SEG_E | SEG_F | SEG_G , SEG_E | SEG_F, SEG_F | SEG_E | SEG_D};
+const uint8_t       SEG_BLINK[]             = {SEG_A | SEG_D | SEG_G, SEG_A | SEG_D | SEG_G, SEG_A | SEG_D | SEG_G, SEG_A | SEG_D | SEG_G};
+TM1637Display       display(CLK, DIO);
 
 
 
+void DisplaySwirl()
+{
+ for (int u = 0; u < 3; u++)
+  {
+   display.setSegments(SEG_SWIRL1);
+   delay(250);
+   display.setSegments(SEG_SWIRL2);
+   delay(250);
+   display.setSegments(SEG_SWIRL3);
+   delay(250);
+   display.setSegments(SEG_SWIRL4);
+   delay(250);
+   display.setSegments(SEG_SWIRL5);
+   delay(250);
+   display.setSegments(SEG_SWIRL6);
+   delay(250);
+  }
 
+}
 
+void DisplayBlink()
+{
+  for (int k = 0; k < 5; k++)
+   {
+     display.setSegments(SEG_BLINK);
+     delay(300);
+     display.clear();
+     delay(200);
+    }
+}
 
 void EnterPassword()
 {
-  if (Password == 0 && (Digit[0] == Password1[0] && Digit[1] == Password1[1] && Digit[2] == Password1[2] && Digit[3] == Password1[3]))
-   {
-     Serial.println("Correct. Step one of two compeleted.");
-     Password = Password +1;
-   }
+   if ((Password == 0 && (Digit[0] != Password1[0] || Digit[1] != Password1[1] || Digit[2] != Password1[2] || Digit[3] != Password1[3])) || (Password == 1 && (Digit[0] != Password2[0] || Digit[1] != Password2[1] || Digit[2] != Password2[2] || Digit[3] != Password2[3]))) 
+     {
+        Attempts = Attempts - 1;
+        Serial.print("Wrong password, you have ");
+        Serial.print((Attempts - 1));
+        Serial.println(" attempt(s) left.");
+        DisplaySwirl();
+        display.setSegments(SEG_FAIL);
+        delay(3000);
+        memset(Digit, 0, sizeof(Digit));
+        memset(data, 0, sizeof(data));
+        display.clear();
+        SelectDigit = 0;
+      }
+
+   if (Password == 0 && (Digit[0] == Password1[0] && Digit[1] == Password1[1] && Digit[2] == Password1[2] && Digit[3] == Password1[3]))
+     {
+        DisplaySwirl();
+        Serial.println("Correct. Step one of two compeleted.");
+        display.setSegments(SEG_PASS);
+        delay(3000);
+        Password = Password +1;
+        memset(Digit, 0, sizeof(Digit));
+        memset(data, 0, sizeof(data));
+        display.clear();
+        SelectDigit = 0;
+      }
   if (Password == 1 && (Digit[0] == Password2[0] && Digit[1] == Password2[1] && Digit[2] == Password2[2] && Digit[3] == Password2[3]))
-   {
-     Serial.println("Bomb has been defused");
-     delay(1000);
-     display.setSegments(SEG_DONE);
-     delay(100000);
-   }
-  else
-  {
-    if ((Password == 0 && (Digit[0] != Password1[0] || Digit[1] != Password1[1] || Digit[2] != Password1[2] || Digit[3] != Password1[3])) || (Password == 1 && (Digit[0] != Password2[0] || Digit[1] != Password2[1] || Digit[2] != Password2[2] || Digit[3] != Password2[3]))) 
-    {
-      Serial.print("Wrong password, you have ");
-      Serial.print((Attempts - 1));
-      Serial.println(" attempt(s) left.");
-      memset(Digit, 0, sizeof(Digit));
-      memset(data, 0, sizeof(data));
-      display.clear();
-      SelectDigit = 0;
-    }
-    Attempts = Attempts - 1;
-  }
+      {
+        Serial.println("Bomb has been defused");
+        DisplaySwirl();
+        display.setSegments(SEG_PASS);
+        delay(2000);
+        DisplayBlink();
+        display.setSegments(SEG_DONE);
+        delay(1000000);
+      }
 }
 
 void DebounceSelect()

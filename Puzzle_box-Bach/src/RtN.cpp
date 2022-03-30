58ffdc2d5ea1696b85ed86355a7b017b465201d6
Harald Hjellen <haralh17@student.uia.no>
2022-03-30 11:28:36 +0200

    RtN

--- Puzzle_box-Bach/src/RtN.cpp
+++ Puzzle_box-Bach/src/RtN.cpp
@@ -102,9 +102,7 @@ int volume = 1;
 unsigned long debounceDelay = 30;
 unsigned long lastDebounceTimeNote = 0;
 
-
 DFRobotDFPlayerMini myDFPlayer;
-void printDetail(uint8_t type, int value);
 
 void setup()
 {
@@ -123,23 +121,7 @@ void setup()
   pinMode(F, INPUT_PULLUP);
   pinMode(G, INPUT_PULLUP);
   pinMode(C, INPUT_PULLUP);
-  
-  
-  
-  Serial.println();
-  Serial.println(F("DFRobot DFPlayer Mini Demo"));
-  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
-  
-  if (!myDFPlayer.begin(Serial1)) {  //Use softwareSerial to communicate with mp3.
-    Serial.println(F("Unable to begin:"));
-    Serial.println(F("1.Please recheck the connection!"));
-    Serial.println(F("2.Please insert the SD card!"));
-    while(true){
-      delay(0); // Code to compatible with ESP8266 watch dog.
-    }
-  }
-  Serial.println(F("DFPlayer Mini online."));
-  
+
   myDFPlayer.volume(volume);  //Set volume value to the volume int
   myDFPlayer.play(13);  //Play the first melody
 }

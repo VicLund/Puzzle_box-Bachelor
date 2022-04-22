875c0c0a0267025e43e5a80619f84fac76920680
Harald Hjellen <haralh17@student.uia.no>
2022-04-22 12:05:14 +0200

    Removed print details

--- Puzzle_box-Bach/src/RtN.cpp
+++ Puzzle_box-Bach/src/RtN.cpp
@@ -116,7 +116,6 @@ Adafruit_NeoPixel stripProgress(LED_COUNT_Progress, LedStripProgressPin, NEO_RGB
 // Argument 3 = Pixel type flags, add together as needed:
 
 DFRobotDFPlayerMini myDFPlayer;
-void printDetail(uint8_t type, int value);
 
 //Define variable to receive
 byte receiveFromMaster = 0;
@@ -152,8 +151,6 @@ void total();
 
 void playRtN();
 
-void printDetail(uint8_t type, int value);
-
 void setup()
 {
   Serial1.begin(9600);
@@ -180,20 +177,6 @@ void setup()
   stripProgress.begin();
   stripProgress.setBrightness(BRIGHTNESS); 
   stripProgress.show();
-  
-  Serial.println();
-  Serial.println(F("DFRobot DFPlayer Mini Demo"));
-  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
-  
-  if (!myDFPlayer.begin(Serial1)) {  //Use softwareSerial to communicate with mp3.
-    Serial.println(F("Unable to begin:"));
-    Serial.println(F("1.Please recheck the connection!"));
-    Serial.println(F("2.Please insert the SD card!"));
-    while(true){
-      delay(0); // Code to compatible with ESP8266 watch dog.
-    }
-  }
-  Serial.println(F("DFPlayer Mini online."));
   
   myDFPlayer.volume(volume);  //Set volume value to the volume int
   myDFPlayer.play(24);
@@ -736,66 +719,4 @@ void playRtN(){
     river(); //check for third melody
     total(); //check for all melodies
   }
-}
-
-void printDetail(uint8_t type, int value){
-  switch (type) {
-    case TimeOut:
-      Serial.println(F("Time Out!"));
-      break;
-    case WrongStack:
-      Serial.println(F("Stack Wrong!"));
-      break;
-    case DFPlayerCardInserted:
-      Serial.println(F("Card Inserted!"));
-      break;
-    case DFPlayerCardRemoved:
-      Serial.println(F("Card Removed!"));
-      break;
-    case DFPlayerCardOnline:
-      Serial.println(F("Card Online!"));
-      break;
-    case DFPlayerUSBInserted:
-      Serial.println("USB Inserted!");
-      break;
-    case DFPlayerUSBRemoved:
-      Serial.println("USB Removed!");
-      break;
-//    case DFPlayerPlayFinished:
-//      Serial.print(F("Number:"));
-//      Serial.print(value);
-//      Serial.println(F(" Play Finished!"));
-      break;
-    case DFPlayerError:
-      Serial.print(F("DFPlayerError:"));
-      switch (value) {
-        case Busy:
-          Serial.println(F("Card not found"));
-          break;
-        case Sleeping:
-          Serial.println(F("Sleeping"));
-          break;
-        case SerialWrongStack:
-          Serial.println(F("Get Wrong Stack"));
-          break;
-        case CheckSumNotMatch:
-          Serial.println(F("Check Sum Not Match"));
-          break;
-        case FileIndexOut:
-          Serial.println(F("File Index Out of Bound"));
-          break;
-        case FileMismatch:
-          Serial.println(F("Cannot Find File"));
-          break;
-        case Advertise:
-          Serial.println(F("In Advertise"));
-          break;
-        default:
-          break;
-      }
-      break;
-    default:
-      break;
-  }
-  
 }

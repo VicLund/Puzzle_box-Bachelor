b8af2b37e9610e4dadb31873e2b41c009a79eb4e
Harald Hjellen <haralh17@student.uia.no>
2022-05-17 10:22:06 +0200

    Last fixes

--- Puzzle_box-Bach/src/Slave1_RtN.cpp
+++ Puzzle_box-Bach/src/Slave1_RtN.cpp
@@ -375,14 +375,15 @@ void debouncec() {
         myDFPlayer.play(1); //play the c note, the file on the SD card with name 0001
         if (R1 == 1 and R2 == 0) { //if c is pressed after R1 = 1, set R2 = 1
           R2 = 1;
-          G1 = 1;
+          G1 = 1, G2 = 0, G3 = 0, G4 = 0, G5 = 0, G6 = 0, G7 = 0, G8 = 0;
+          S1 = 0;
         }
         else if (R13 == 1 and R14 == 0) { //if c is pressed after R13 = 1, set R14 = 1
           R14 = 1;
-          G1 = 1;
+          G1 = 1, G2 = 0, G3 = 0, G4 = 0, G5 = 0, G6 = 0, G7 = 0, G8 = 0;
         }
-        else if (R1 == 1 and R2 == 2 and R3 == 0){
-          G1 = 1;
+        else if (R1 == 1 and R2 == 1 and R3 == 0){
+          G1 = 1, G2 = 0, G3 = 0, G4 = 0, G5 = 0, G6 = 0, G7 = 0, G8 = 0;
         }
         else { //First/Wrong button, reset all variables
           R1 = 1, R2 = 0, R3 = 0, R4 = 0, R5 = 0, R6 = 0, R7 = 0, R8 = 0, R9 = 0, R10 = 0, R11 = 0, R12 = 0, R13 = 0, R14 = 0;
@@ -422,6 +423,8 @@ void debounceD() {
         }
         else if (G2 == 1 and G3 == 0){ //if D is pressed after G2 = 1, set G3 = 1
           G3 = 1;
+          R3 = 0;
+          S2 = 0;
         }
         else { //Wrong button, reset all variables
           R1 = 0, R2 = 0, R3 = 0, R4 = 0, R5 = 0, R6 = 0, R7 = 0, R8 = 0, R9 = 0, R10 = 0, R11 = 0, R12 = 0, R13 = 0, R14 = 0;
@@ -524,14 +527,16 @@ void debounceG() {
     if (G_reading != G_state) {
       G_state = G_reading;
       if (G_state == LOW) {
-        myDFPlayer.play(5); //play the G note
+        myDFPlayer.play(5); //play the G note        
         if (R2 == 1 and R3 == 0) { //check if user press the correct button combination
           R3 = 1;
           S1 = 1;
+          G2 = 1;
         }
         else if (R3 == 1 and R4 == 0) {
           R4 = 1;
-          S1 = 1;
+          S2 = 1;
+          G1 = 0, G2 = 0;
         }
         else if (R6 == 1 and R7 == 0) {
           R7 = 1;
@@ -546,7 +551,10 @@ void debounceG() {
           S1 = 1;
         }
         else if (S1 == 1 and S2 == 0) {
-          S2 = 1;
+          S2 = 1, G1 = 0, G2 = 0;
+        }
+        else if (S1 == 1 and S2 == 1 and S3 == 0){
+          S2 = 1, G1 = 0, G2 = 0;
         }
         else if (S4 == 1 and S5 == 0) {
           S5 = 1;
@@ -564,7 +572,7 @@ void debounceG() {
           G2 = 1;
           S1 = 1;
         }
-        else { //First/Wrong button, reset all variables
+        else {//First/Wrong button, reset all variables
           R1 = 0, R2 = 0, R3 = 0, R4 = 0, R5 = 0, R6 = 0, R7 = 0, R8 = 0, R9 = 0, R10 = 0, R11 = 0, R12 = 0, R13 = 0, R14 = 0;
           S1 = 1, S2 = 0, S3 = 0, S4 = 0, S5 = 0, S6 = 0, S7 = 0, S8 = 0, S9 = 0, S10 = 0, S11 = 0, S12 = 0, S13 = 0;
           Z1 = 0, Z2 = 0, Z3 = 0, Z4 = 0, Z5 = 0, Z6 = 0, Z7 = 0, Z8 = 0, Z9 = 0, Z10 = 0, Z11 = 0, Z12 = 0, Z13 = 0;
@@ -589,10 +597,13 @@ void debounceA() {
       if (A_state == LOW) {
         myDFPlayer.play(6); //play the A note
         if (R4 == 1 and R5 == 0) { //check if user press the correct button combination
-          R5 = 1;
+          R5 = 1, S3 = 1;
         }
         else if (R5 == 1 and R6 == 0) {
-          R6 = 1;
+          R6 = 1, S3 = 0, S2 = 0, S1 = 0;
+        }
+        else if (S2 == 1 and S3 == 0 and G3 == 1 and G4 == 0) {
+          S3 = 1, G4 = 1;
         }
         else if (S2 == 1 and S3 == 0) {
           S3 = 1;
@@ -637,7 +648,7 @@ void debounceB() {
           Z4 = 1;
         }
         else if (S3 == 1 and S4 == 0) {
-          S4 = 1;
+          S4 = 1, R1 = 0, R2 = 0, R3 = 0, R4 = 0, R5 = 0;
         }
         else if (S5 == 1 and S6 == 0) {
           S6 = 1;
